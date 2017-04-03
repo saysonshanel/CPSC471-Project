136ffbc38fa8708df32f388901eb96158095115e
saysonshanel <ssayson@ucalgary.ca>
2017-04-02 20:33:45 -0700

    typo

--- Config.php
+++ Config.php
@@ -2,6 +2,6 @@
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', 'rootpassword');
-    define('DB_DATABASE', 'darabase');
+    define('DB_DATABASE', 'database');
    ?>
 

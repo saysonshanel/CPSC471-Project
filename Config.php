b1f126678860ed0f6a501f30ff64c8e5e0f9d0a5
saysonshanel <ssayson@ucalgary.ca>
2017-04-05 15:30:30 -0700

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,7 +1,10 @@
 <?php
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', 'rootpassword');
-    define('DB_DATABASE', 'database');
+    define('DB_PASSWORD', 'groupProj11');
+    define('DB_DATABASE', 'dealership');
+
+    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
    ?>
 
